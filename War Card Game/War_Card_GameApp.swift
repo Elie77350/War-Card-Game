07a94167a1042a5116be56da209ef6fade45742d
Elie BINGUE <eliebingue7@icloud.com>
2024-05-10 12:26:57 +0200

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Elie BINGUE on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
